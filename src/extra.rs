077288be2383d475a09802d65509c798be498b29
dashaw92 <dashaw92@gmail.com>
2024-09-23 13:21:43 -0400

    Add anyhow context to all try ops in dan_world

--- src/extra.rs
+++ src/extra.rs
@@ -1,3 +1,5 @@
+use anyhow::Context;
+use anyhow::Result;
 use byteorder::BigEndian;
 
 use crate::read_string;
@@ -8,14 +10,12 @@ pub struct DanExtra {
 }
 
 impl DanExtra {
-    pub(crate) fn from_buf<R: byteorder::ReadBytesExt>(
-        r: &mut R,
-    ) -> std::io::Result<(String, DanExtra)> {
-        let key = read_string(r)?;
-        let len = r.read_u16::<BigEndian>()? as usize;
+    pub(crate) fn from_buf<R: byteorder::ReadBytesExt>(r: &mut R) -> Result<(String, DanExtra)> {
+        let key = read_string(r).context("extra key")?;
+        let len = r.read_u16::<BigEndian>().context("extra data len")? as usize;
         let mut data = Vec::with_capacity(len);
         for _ in 0..len {
-            data.push(r.read_u8()?);
+            data.push(r.read_u8().context("byte for extra data")?);
         }
 
         Ok((key, DanExtra { data }))
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,12 @@
 
 use std::{
     collections::HashMap,
-    io::{self, Read, Result},
+    io::{self, Read},
     path::Path,
 };
 
+use anyhow::Context;
+use anyhow::Result;
 use blockdata::DanBlockData;
 use byteorder::{BigEndian, ReadBytesExt};
 
@@ -51,33 +53,35 @@ pub struct DanChunkSection {
 
 impl DanWorld {
     pub fn load<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let bytes = std::fs::read(path)?;
+        let bytes = std::fs::read(path).context("Loading file")?;
         let mut gz = GzDecoder::new(&bytes[..]);
 
-        let _magic = read_string(&mut gz)?;
+        let _magic = read_string(&mut gz).context("Reading magic")?;
         assert_eq!(&_magic, "DanWorld");
-        let version = gz.read_u8()?;
+        let version = gz.read_u8().context("Reading version")?;
 
-        let dimension = match gz.read_u8()? {
+        let dimension = match gz.read_u8().context("Reading dimension")? {
             0 => DanDimension::Overworld,
             1 => DanDimension::Nether,
             2 => DanDimension::End,
             _ => DanDimension::Overworld,
         };
 
-        let width = gz.read_u16::<BigEndian>()?;
-        let depth = gz.read_u16::<BigEndian>()?;
+        let width = gz.read_u16::<BigEndian>().context("Width")?;
+        let depth = gz.read_u16::<BigEndian>().context("Depth")?;
 
         let mut chunks = Vec::with_capacity((width * depth) as usize);
 
         for _ in 0..chunks.capacity() {
-            chunks.push(read_chunk(&mut gz)?);
+            chunks.push(read_chunk(&mut gz).context("Reading chunk")?);
         }
 
-        let num_extra = gz.read_u32::<BigEndian>()? as usize;
+        let num_extra = gz
+            .read_u32::<BigEndian>()
+            .context("Number of extra values")? as usize;
         let mut extra = HashMap::with_capacity(num_extra);
         for _ in 0..num_extra {
-            let (key, data) = DanExtra::from_buf(&mut gz)?;
+            let (key, data) = DanExtra::from_buf(&mut gz).context("Loading extra")?;
             extra.insert(key, data);
         }
 
@@ -99,39 +103,44 @@ impl DanWorld {
 type Cur<'a> = GzDecoder<&'a [u8]>;
 
 fn read_chunk(c: &mut Cur) -> Result<DanChunk> {
-    let x = c.read_u16::<BigEndian>()?;
-    let z = c.read_u16::<BigEndian>()?;
+    let x = c.read_u16::<BigEndian>().context("chunk x")?;
+    let z = c.read_u16::<BigEndian>().context("chunk z")?;
 
     let mut sections = Vec::with_capacity(8);
 
-    let num_sections = c.read_u8()?;
+    let num_sections = c.read_u8().context("number of chunk sections")?;
     for _ in 0..num_sections {
-        sections.push(read_chunk_section(c)?);
+        sections.push(read_chunk_section(c).context("reading chunk section")?);
     }
 
     Ok(DanChunk { x, z, sections })
 }
 
 fn read_chunk_section(c: &mut Cur) -> Result<DanChunkSection> {
-    let palette_len = c.read_u8()?;
+    let palette_len = c.read_u8().context("palette length")?;
     let mut palette = Vec::with_capacity(palette_len as usize);
 
     for _ in 0..palette_len {
-        palette.push(read_string(c)?);
+        palette.push(read_string(c).context("reading palette")?);
     }
 
-    let num_blocks = c.read_u16::<BigEndian>()? as usize;
+    let num_blocks = c
+        .read_u16::<BigEndian>()
+        .context("number of blocks in section")? as usize;
     let mut blocks = vec![0u8; num_blocks];
-    c.read_exact(&mut blocks)?;
+    c.read_exact(&mut blocks)
+        .context("filling chunk section block array")?;
 
     let mut biomes = vec![0u8; num_blocks];
-    c.read_exact(&mut biomes)?;
+    c.read_exact(&mut biomes).context("reading biomes")?;
     let biomes = biomes.into_iter().map(DanBiome::from).collect();
 
-    let num_data = c.read_u16::<BigEndian>()? as usize;
+    let num_data = c
+        .read_u16::<BigEndian>()
+        .context("number of block data elements in section")? as usize;
     let mut data = HashMap::with_capacity(num_data);
     for _ in 0..num_data {
-        let block_data_bits = c.read_u16::<BigEndian>()? as usize;
+        let block_data_bits = c.read_u16::<BigEndian>().context("block data bits")? as usize;
 
         let block_x = (block_data_bits & 0b1111_0000_0000_0000) >> 12;
         let block_y = (block_data_bits & 0b0000_1111_0000_0000) >> 8;
@@ -140,7 +149,10 @@ fn read_chunk_section(c: &mut Cur) -> Result<DanChunkSection> {
         let mut current_data = Vec::with_capacity(data_len);
 
         for _ in 0..data_len {
-            let Some(current) = blockdata::from(c.read_u16::<BigEndian>()?) else {
+            let Some(current) = blockdata::from(
+                c.read_u16::<BigEndian>()
+                    .context("reading block data element")?,
+            ) else {
                 continue;
             };
 
@@ -163,7 +175,9 @@ fn read_chunk_section(c: &mut Cur) -> Result<DanChunkSection> {
 pub(crate) fn read_string<W: ReadBytesExt>(c: &mut W) -> Result<String> {
     let len = c.read_u8()? as usize;
     let mut buf = vec![0; len];
-    c.read_exact(&mut buf)?;
+    c.read_exact(&mut buf).context("String length")?;
 
-    String::from_utf8(buf).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+    String::from_utf8(buf)
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+        .context("Reading string")
 }
