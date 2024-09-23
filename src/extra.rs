34dff6f0a5c8d46ff4ec12b764c9a592b61713f9
dashaw92 <dashaw92@gmail.com>
2024-09-23 15:35:02 -0400

    Helpers for using extra data in rust

--- src/extra.rs
+++ src/extra.rs
@@ -1,6 +1,9 @@
+use std::io::Cursor;
+
 use anyhow::Context;
 use anyhow::Result;
 use byteorder::BigEndian;
+use byteorder::ReadBytesExt;
 
 use crate::read_string;
 
@@ -19,5 +22,42 @@ impl DanExtra {
         }
 
         Ok((key, DanExtra { data }))
+    }
+
+    pub fn to_coords(&self) -> anyhow::Result<[f64; 5]> {
+        let mut cursor = Cursor::new(&self.data);
+        let mut buf = [0.0; 5];
+
+        for i in 0..3 {
+            buf[i] = cursor
+                .read_f64::<BigEndian>()
+                .context("Expected bytes to be floats")?;
+        }
+
+        for i in 3..5 {
+            buf[i] = cursor
+                .read_f32::<BigEndian>()
+                .context("Reading yaw and pitch in extra")? as f64;
+        }
+
+        Ok(buf)
+    }
+
+    pub fn to_string(&self) -> anyhow::Result<String> {
+        let mut cursor = Cursor::new(&self.data);
+
+        let len = cursor
+            .read_u32::<BigEndian>()
+            .context("Reading string length from extra")? as usize;
+        let mut buf = Vec::with_capacity(len);
+        for _ in 0..len {
+            buf.push(
+                cursor
+                    .read_u8()
+                    .context("reading byte from string from extra")?,
+            );
+        }
+
+        String::from_utf8(buf).context("Converting raw UTF-8 bytes to a string from extra")
     }
 }
